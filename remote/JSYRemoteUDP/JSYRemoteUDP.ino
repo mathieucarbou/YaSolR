b757b34532214d6e031db1bd8a01efd6d66d05df
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-06-24 00:54:57 +0200

    JSY Remote through UDP - Client side
    (cherry picked from commit 85ca5ebf2db38d352a125cdbb6eaac46fb14e323)

--- remote/JSYRemoteUDP/JSYRemoteUDP.ino
+++ remote/JSYRemoteUDP/JSYRemoteUDP.ino
@@ -1,34 +1,35 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <MycilaJSY.h>
+#include <MycilaLogger.h>
 
 Mycila::JSY jsy;
+Mycila::Logger logger;
 
 void setup() {
   Serial.begin(115200);
   while (!Serial)
     continue;
 
+  logger.setLevel(ARDUHAL_LOG_LEVEL_DEBUG);
+  logger.forwardTo(&Serial);
+
   // read JSY on pins 17 (JSY RX / Serial TX) and 16 (JSY TX / Serial RX)
-  jsy.begin(Serial2, 16, 17, true);
+  jsy.begin(Serial2, 16, 17);
+  if (jsy.isEnabled() && jsy.getBaudRate() != Mycila::JSYBaudRate::BAUD_38400)
+    jsy.setBaudRate(Mycila::JSYBaudRate::BAUD_38400);
 }
 
-Mycila::JSYBaudRate target = Mycila::JSYBaudRate::BAUD_38400;
-
 void loop() {
-  if (!jsy.isEnabled()) {
-    delay(1000);
-    return;
-  }
+  jsy.read();
 
+  if (jsy.isEnabled()) {
+    Serial.println((int)jsy.getBaudRate());
+
+    JsonDocument doc;
+    jsy.toJson(doc.to<JsonObject>());
+    serializeJson(doc, Serial);
+    Serial.println();
+  }
   delay(1000);
-
-  JsonDocument doc;
-  jsy.toJson(doc.to<JsonObject>());
-  serializeJson(doc, Serial);
-  Serial.println();
-
-  if (jsy.getBaudRate() != target) {
-    jsy.setBaudRate(target);
-  }
 }
