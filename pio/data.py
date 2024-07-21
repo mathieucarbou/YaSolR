321cee953411c454cb1048ed8008105b8b10794e
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-07-21 23:17:49 +0200

    Partition file update
    (cherry picked from commit d9496d15974b4884ce7d0419a200115aec75b909)

--- pio/data.py
+++ pio/data.py
@@ -2,15 +2,19 @@ import gzip
 import os
 
 os.makedirs('.pio/data', exist_ok=True)
+
 for filename in ['config.html', 'logo-icon.png', 'logo.png']:
     skip = False
+
     if os.path.isfile('.pio/data/' + filename + '.timestamp'):
         with open('.pio/data/' + filename + '.timestamp', 'r', -1, 'utf-8') as timestampFile:
             if os.path.getmtime('data/' + filename) == float(timestampFile.readline()):
                 skip = True
+
     if skip:
         print('[data.py] ' + filename + ' up to date')
         continue
+
     with open('data/' + filename, 'rb') as inputFile:
         with gzip.open('.pio/data/' + filename + '.gz', 'wb') as outputFile:
             print('[data.py] gzip \'data/' + filename + '\' to \'.pio/data/' + filename + '.gz\'')
--- pio/factory.py
+++ pio/factory.py
@@ -17,27 +17,40 @@
 #   0x10000 | firmware.bin
 #  0x3F0000 | littlefs.bin
 
-Import("env")
-
-platform = env.PioPlatform()
+Import("env", "projenv")
 
 import sys
 from os.path import join, getsize
 
-sys.path.append(join(platform.get_package_dir("tool-esptoolpy")))
+sys.path.append(join(env.PioPlatform().get_package_dir("tool-esptoolpy")))
 import esptool
+
+# print(env.Dump())
+# print(projenv.Dump())
 
 def esp32_create_combined_bin(source, target, env):
     print("Generating combined binary for serial flashing")
 
+    # env.Execute('pio run -t buildfs -e %s' % env['PIOENV'])
+
     # The offset from begin of the file where the app0 partition starts
     # This is defined in the partition .csv file
     app_offset = 0x10000
+    fs_offset = 0x0
+
+    if env.get("PARTITIONS_TABLE_CSV").endswith("partitions-4MB.csv"):
+        fs_offset = 0x3F0000
+    if env.get("PARTITIONS_TABLE_CSV").endswith("partitions-8MB.csv"):
+        fs_offset = 0x7D0000
+
+    if fs_offset == 0:
+        print(env.get("PARTITIONS_TABLE_CSV"))
+        raise Exception("Unknown partition file, cannot determine FS offset")
 
     new_file_name = env.subst("$BUILD_DIR/${PROGNAME}.factory.bin")
     sections = env.subst(env.get("FLASH_EXTRA_IMAGES"))
     firmware_name = env.subst("$BUILD_DIR/${PROGNAME}.bin")
-    # fs_name = env.subst("$BUILD_DIR/littlefs.bin")
+    fs_name = env.subst("$BUILD_DIR/littlefs.bin")
     chip = env.get("BOARD_MCU")
     flash_size = env.BoardConfig().get("upload.flash_size")
     flash_freq = env.BoardConfig().get("build.f_flash", "40m")
@@ -82,7 +95,7 @@ def esp32_create_combined_bin(source, target, env):
     # print(f" - {hex(fs_offset)} | {fs_name}")
     # cmd += [hex(fs_offset), fs_name]
 
-    print('Using esptool.py arguments: %s' % ' '.join(cmd))
+    print("Using esptool.py arguments: %s" % " ".join(cmd))
 
     esptool.main(cmd)
 
