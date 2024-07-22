da6384e3c84dc53fd82edf1f308da24d55117f51
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-07-22 10:33:16 +0200

    speed up pid tuning
    (cherry picked from commit dd5476b6819250ed52e5aa490c2fd1387577fb21)

--- src/tasks/Router.cpp
+++ src/tasks/Router.cpp
@@ -47,9 +47,11 @@ Mycila::Task routingTask("Routing", Mycila::TaskType::ONCE, [](void* params) {
 
     if (config.getBool(KEY_ENABLE_PID_VIEW)) {
       YaSolR::Website.updatePID();
-      AsyncWebSocketMessageBuffer* buffer = wsDebugPID.makeBuffer(256);
-      snprintf((char*)buffer->get(), 256, "%d,%d,%d,%d,%d,%.3f,%.3f,%.3f,%d,%d,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f", pidController.getProportionalMode(), pidController.getDerivativeMode(), pidController.getIntegralCorrectionMode(), pidController.isReversed(), static_cast<int>(pidController.getSetPoint()), pidController.getKp(), pidController.getKi(), pidController.getKd(), static_cast<int>(pidController.getOutputMin()), static_cast<int>(pidController.getOutputMax()), pidController.getInput(), pidController.getOutput(), pidController.getError(), pidController.getSum(), pidController.getPTerm(), pidController.getITerm(), pidController.getDTerm()); // NOLINT
-      wsDebugPID.textAll(buffer);
+      if (wsDebugPID.count()) {
+        AsyncWebSocketMessageBuffer* buffer = wsDebugPID.makeBuffer(256);
+        snprintf((char*)buffer->get(), 256, "%d,%d,%d,%d,%d,%.3f,%.3f,%.3f,%d,%d,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f", pidController.getProportionalMode(), pidController.getDerivativeMode(), pidController.getIntegralCorrectionMode(), pidController.isReversed(), static_cast<int>(pidController.getSetPoint()), pidController.getKp(), pidController.getKi(), pidController.getKd(), static_cast<int>(pidController.getOutputMin()), static_cast<int>(pidController.getOutputMax()), pidController.getInput(), pidController.getOutput(), pidController.getError(), pidController.getSum(), pidController.getPTerm(), pidController.getITerm(), pidController.getDTerm()); // NOLINT
+        wsDebugPID.textAll(buffer);
+      }
     }
   }
 });
