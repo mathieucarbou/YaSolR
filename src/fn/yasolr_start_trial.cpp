40ceeb758a96491685a343ee819bb64591e0eb18
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-12-29 13:29:52 +0100

    trial expired checked when starting trial

--- src/fn/yasolr_start_trial.cpp
+++ src/fn/yasolr_start_trial.cpp
@@ -10,6 +10,9 @@ Mycila::Task* trialTask;
 
 void yasolr_start_trial() {
 #ifdef APP_MODEL_TRIAL
+  Mycila::Trial.begin();
+  Mycila::Trial.validate();
+
   trialTask = new Mycila::Task("Trial", [](void* params) { Mycila::Trial.validate(); });
   trialTask->setInterval(30 * Mycila::TaskDuration::SECONDS);
   trialTask->setManager(coreTaskManager);
