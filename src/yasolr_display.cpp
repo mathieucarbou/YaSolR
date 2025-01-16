8ed0c788df2869083db393efc29cd8c595013afa
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-01-16 01:20:22 +0100

    Reduce screen refresh interval

--- src/yasolr_display.cpp
+++ src/yasolr_display.cpp
@@ -216,7 +216,7 @@ void yasolr_init_display() {
     });
 
     // refresh each 500ms if scrolling speed is 1 second, else 1 second
-    displayTask->setInterval(200 * Mycila::TaskDuration::MILLISECONDS);
+    displayTask->setInterval(1000 * Mycila::TaskDuration::MILLISECONDS);
     displayTask->setManager(coreTaskManager);
     if (config.getBool(KEY_ENABLE_DEBUG))
       displayTask->enableProfiling(10, Mycila::TaskTimeUnit::MILLISECONDS);
