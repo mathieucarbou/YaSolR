898857ef89ae011a1483a001fa4a6bb456e82007
Mathieu Carbou <mathieu.carbou@gmail.com>
2025-09-11 14:14:19 +0200

    Lower startup logs size in non-debug mode

--- src/yasolr_logging.cpp
+++ src/yasolr_logging.cpp
@@ -80,7 +80,7 @@ void yasolr_init_startup_logging() {
     LOGI(TAG, "Previous log file removed");
 
   LOGI(TAG, "Redirecting logs to " YASOLR_LOG_FILE);
-  logStream = new LogStream(YASOLR_LOG_FILE, 16 * 1024, []() {
+  logStream = new LogStream(YASOLR_LOG_FILE, (config.getBool(KEY_ENABLE_DEBUG) ? 16 : 8) * 1024, []() {
     delete logStream;
     logStream = nullptr;
     LOGW(TAG, "Startup log size limit reached!");
