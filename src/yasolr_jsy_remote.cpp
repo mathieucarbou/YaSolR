1c5cff1bc16441ee79ff95c0c1b9821137574dec
Mathieu Carbou <mathieu.carbou@gmail.com>
2026-01-24 15:24:05 +0100

    Troubleshoot JSY Remote issue

--- src/yasolr_jsy_remote.cpp
+++ src/yasolr_jsy_remote.cpp
@@ -7,6 +7,14 @@
 #include <algorithm>
 #include <utility>
 
+#define YASOLR_DEBUG_UDP 0
+
+#if YASOLR_DEBUG_UDP
+  #define DEBUG_UDP(...) ESP_LOGD(__VA_ARGS__)
+#else
+  #define DEBUG_UDP(...)
+#endif
+
 AsyncUDP* udp = nullptr;
 Mycila::CircularBuffer<float, 15>* udpMessageRateBuffer = nullptr;
 Mycila::Task* jsyRemoteTask = nullptr;
@@ -49,62 +57,70 @@ class UDPMessage {
 static uint32_t lastMessageID = 0;
 static UDPMessage* reassembledMessage = nullptr;
 
-static void processJSON(const JsonDocument& doc) {
+static void processJSON(JsonDocument& doc) {
   Mycila::Grid::Metrics metrics;
   metrics.source = Mycila::Grid::Source::JSY_REMOTE;
+  JsonObject root = doc.as<JsonObject>();
 
-  // ESP_LOGD(TAG, "[UDP] JSY Model: %" PRIu16, doc["model"].as<uint16_t>());
+#if YASOLR_DEBUG_UDP
+  const size_t jsonSize = measureJson(doc);
+  String jsonString;
+  jsonString.reserve(jsonSize + 1);
+  serializeJson(root, jsonString);
+  DEBUG_UDP(TAG, "[UDP] JSY Data: %s", jsonString.c_str());
+  DEBUG_UDP(TAG, "[UDP] JSY Model: %" PRIu16, root["model"].as<uint16_t>());
+#endif
 
-  switch (doc["model"].as<uint16_t>()) {
+  switch (root["model"].as<uint16_t>()) {
     case MYCILA_JSY_MK_163:
     case MYCILA_JSY_MK_227:
     case MYCILA_JSY_MK_229: {
-      metrics.apparentPower = doc["apparent_power"] | NAN;
-      metrics.current = doc["current"] | NAN;
-      metrics.energy = doc["active_energy_imported"] | static_cast<uint32_t>(0);
-      metrics.energyReturned = doc["active_energy_returned"] | static_cast<uint32_t>(0);
-      metrics.frequency = doc["frequency"] | NAN;
-      metrics.power = doc["active_power"] | NAN;
-      metrics.powerFactor = doc["power_factor"] | NAN;
-      metrics.voltage = doc["voltage"] | NAN;
+      metrics.apparentPower = root["apparent_power"] | NAN;
+      metrics.current = root["current"] | NAN;
+      metrics.energy = root["active_energy_imported"] | static_cast<uint32_t>(0);
+      metrics.energyReturned = root["active_energy_returned"] | static_cast<uint32_t>(0);
+      metrics.frequency = root["frequency"] | NAN;
+      metrics.power = root["active_power"] | NAN;
+      metrics.powerFactor = root["power_factor"] | NAN;
+      metrics.voltage = root["voltage"] | NAN;
       break;
     }
     case MYCILA_JSY_MK_193:
     case MYCILA_JSY_MK_194: {
-      if (doc["channel1"].is<JsonObject>()) {
+      if (root["channel1"].is<JsonObject>()) {
         Mycila::Router::Metrics routerMetrics;
         routerMetrics.source = Mycila::Router::Source::JSY_REMOTE;
-        routerMetrics.apparentPower = doc["channel1"]["apparent_power"] | NAN;
-        routerMetrics.current = doc["channel1"]["current"] | NAN;
-        routerMetrics.energy = (doc["channel1"]["active_energy"] | static_cast<uint32_t>(0)) + (doc["channel1"]["active_energy_returned"] | static_cast<uint32_t>(0)); // if the clamp is installed reversed
-        routerMetrics.power = std::abs(doc["channel1"]["active_power"] | NAN);                                                                                         // if the clamp is installed reversed
-        routerMetrics.powerFactor = doc["channel1"]["power_factor"] | NAN;
-        routerMetrics.resistance = doc["channel1"]["resistance"] | NAN;
-        routerMetrics.thdi = doc["channel1"]["thdi_0"] | NAN;
-        routerMetrics.voltage = doc["channel1"]["dimmed_voltage"] | NAN;
+        routerMetrics.apparentPower = root["channel1"]["apparent_power"] | NAN;
+        routerMetrics.current = root["channel1"]["current"] | NAN;
+        routerMetrics.energy = (root["channel1"]["active_energy"] | static_cast<uint32_t>(0)) + (root["channel1"]["active_energy_returned"] | static_cast<uint32_t>(0)); // if the clamp is installed reversed
+        routerMetrics.power = std::abs(root["channel1"]["active_power"] | NAN);                                                                                          // if the clamp is installed reversed
+        routerMetrics.powerFactor = root["channel1"]["power_factor"] | NAN;
+        routerMetrics.resistance = root["channel1"]["resistance"] | NAN;
+        routerMetrics.thdi = root["channel1"]["thdi_0"] | NAN;
+        routerMetrics.voltage = root["channel1"]["dimmed_voltage"] | NAN;
         router.updateMetrics(std::move(routerMetrics));
       }
-      if (doc["channel2"].is<JsonObject>()) {
-        metrics.apparentPower = doc["channel2"]["apparent_power"] | NAN;
-        metrics.current = doc["channel2"]["current"] | NAN;
-        metrics.energy = doc["channel2"]["active_energy_imported"] | static_cast<uint32_t>(0);
-        metrics.energyReturned = doc["channel2"]["active_energy_returned"] | static_cast<uint32_t>(0);
-        metrics.frequency = doc["channel2"]["frequency"] | NAN;
-        metrics.power = doc["channel2"]["active_power"] | NAN;
-        metrics.powerFactor = doc["channel2"]["power_factor"] | NAN;
-        metrics.voltage = doc["channel2"]["voltage"] | NAN;
+      if (root["channel2"].is<JsonObject>()) {
+        metrics.apparentPower = root["channel2"]["apparent_power"] | NAN;
+        metrics.current = root["channel2"]["current"] | NAN;
+        metrics.energy = root["channel2"]["active_energy_imported"] | static_cast<uint32_t>(0);
+        metrics.energyReturned = root["channel2"]["active_energy_returned"] | static_cast<uint32_t>(0);
+        metrics.frequency = root["channel2"]["frequency"] | NAN;
+        metrics.power = root["channel2"]["active_power"] | NAN;
+        metrics.powerFactor = root["channel2"]["power_factor"] | NAN;
+        metrics.voltage = root["channel2"]["voltage"] | NAN;
       }
       break;
     }
     case MYCILA_JSY_MK_333: {
-      metrics.apparentPower = doc["aggregate"]["apparent_power"] | NAN;
-      metrics.current = doc["aggregate"]["current"] | NAN;
-      metrics.energy = doc["aggregate"]["active_energy_imported"] | static_cast<uint32_t>(0);
-      metrics.energyReturned = doc["aggregate"]["active_energy_returned"] | static_cast<uint32_t>(0);
-      metrics.frequency = doc["aggregate"]["frequency"] | NAN;
-      metrics.power = doc["aggregate"]["active_power"] | NAN;
-      metrics.powerFactor = doc["aggregate"]["power_factor"] | NAN;
-      metrics.voltage = doc["aggregate"]["voltage"] | NAN;
+      metrics.apparentPower = root["aggregate"]["apparent_power"] | NAN;
+      metrics.current = root["aggregate"]["current"] | NAN;
+      metrics.energy = root["aggregate"]["active_energy_imported"] | static_cast<uint32_t>(0);
+      metrics.energyReturned = root["aggregate"]["active_energy_returned"] | static_cast<uint32_t>(0);
+      metrics.frequency = root["aggregate"]["frequency"] | NAN;
+      metrics.power = root["aggregate"]["active_power"] | NAN;
+      metrics.powerFactor = root["aggregate"]["power_factor"] | NAN;
+      metrics.voltage = root["aggregate"]["voltage"] | NAN;
       break;
     }
     default:
@@ -122,7 +138,7 @@ static void processJSON(const JsonDocument& doc) {
 }
 
 static void onData(AsyncUDPPacket& packet) {
-  // buffer[0] == MYCILA_UDP_MSG_TYPE_JSY_DATA (1)
+  // buffer[0] == YASOLR_UDP_MSG_TYPE_JSY_DATA (1)
   // buffer[1] == message ID (4) - uint32_t
   // buffer[5] == jsonSize (4) - size_t
   // buffer[9] == MsgPack (?)
@@ -131,7 +147,7 @@ static void onData(AsyncUDPPacket& packet) {
   const size_t len = packet.length();
   const uint8_t* buffer = packet.data();
 
-  // ESP_LOGD(TAG, "[UDP] Received packet of size %" PRIu32, len);
+  DEBUG_UDP(TAG, "[UDP] Received packet of size %" PRIu32, len);
 
   // if we are waiting for more packets, check for timeout (10 seconds) in case a sender disappears mid-message
   // we free the buffer and reset state before processing the new packet
@@ -147,6 +163,8 @@ static void onData(AsyncUDPPacket& packet) {
     if (len <= 13 || buffer[0] != YASOLR_UDP_MSG_TYPE_JSY_DATA) {
       ESP_LOGD(TAG, "[UDP] Invalid packet received of size %" PRIu32 ", not coming from a supported Mycila JSY App version!", len);
       return;
+    } else {
+      DEBUG_UDP(TAG, "[UDP] New MycilaJSYApp packet detected!");
     }
 
     // extract message ID
@@ -160,29 +178,26 @@ static void onData(AsyncUDPPacket& packet) {
     }
 
     // extract message size
-    size_t jsonSize = 0;
-    memcpy(&jsonSize, buffer + 5, 4);
+    size_t payloadSize = 0;
+    memcpy(&payloadSize, buffer + 5, 4);
 
     // validate message size
-    if (!jsonSize) {
+    if (!payloadSize) {
       ESP_LOGD(TAG, "[UDP] Invalid message size: 0");
       return;
+    } else {
+      DEBUG_UDP(TAG, "[UDP] Internal payload size: %" PRIu32, payloadSize);
     }
 
     // arbitrary limit to avoid memory exhaustion
-    if (jsonSize > 4096) {
-      ESP_LOGD(TAG, "[UDP] Message size too large: %" PRIu32, jsonSize);
+    if (payloadSize > 4096) {
+      ESP_LOGD(TAG, "[UDP] Message size too large: %" PRIu32, payloadSize);
       return;
     }
 
     // compute total reassembled message size and allocate buffer
-    // ESP_LOGD(TAG, "[UDP] Allocating new message buffer of size %" PRIu32, jsonSize + 13);
-    reassembledMessage = new (std::nothrow) UDPMessage(jsonSize);
-
-    if (reassembledMessage == nullptr) {
-      ESP_LOGD(TAG, "[UDP] Failed to allocate memory for data size %" PRIu32, jsonSize);
-      return;
-    }
+    reassembledMessage = new UDPMessage(payloadSize);
+    DEBUG_UDP(TAG, "[UDP] Allocated new message buffer of size %" PRIu32, reassembledMessage->remaining);
 
     // save last message ID
     lastMessageID = messageID;
@@ -197,7 +212,7 @@ static void onData(AsyncUDPPacket& packet) {
     }
 
   } else {
-    // ESP_LOGD(TAG, "[UDP] Validating next packet");
+    DEBUG_UDP(TAG, "[UDP] Validating next packet");
 
     // check that the packet comes from the same sender
     uint8_t mac[6];
@@ -205,12 +220,16 @@ static void onData(AsyncUDPPacket& packet) {
     if (memcmp(mac, reassembledMessage->sourceMAC, 6) != 0) {
       ESP_LOGD(TAG, "[UDP] Discarding packet from different sender. Expected: %02X:%02X:%02X:%02X:%02X:%02X, got %02X:%02X:%02X:%02X:%02X:%02X", reassembledMessage->sourceMAC[0], reassembledMessage->sourceMAC[1], reassembledMessage->sourceMAC[2], reassembledMessage->sourceMAC[3], reassembledMessage->sourceMAC[4], reassembledMessage->sourceMAC[5], mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
       return;
+    } else {
+      DEBUG_UDP(TAG, "[UDP] Packet sender validated: %02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
     }
 
     // check if the packet comes by the same interface
     if (packet.localIP() != reassembledMessage->interface && packet.localIPv6() != reassembledMessage->interface) {
       ESP_LOGD(TAG, "[UDP] Discarding packet from different interface. Expected: %s, got IPv4: %s, IPv6: %s", reassembledMessage->interface.toString().c_str(), packet.localIP().toString().c_str(), packet.localIPv6().toString().c_str());
       return;
+    } else {
+      DEBUG_UDP(TAG, "[UDP] Packet interface validated");
     }
 
     // additional packet validated
@@ -221,8 +240,10 @@ static void onData(AsyncUDPPacket& packet) {
   reassembledMessage->append(buffer, len);
 
   if (reassembledMessage->remaining) {
-    // ESP_LOGD(TAG, "[UDP] Waiting for more packets to complete message. Remaining size: %" PRIu32, reassembledMessage->remaining);
+    DEBUG_UDP(TAG, "[UDP] Waiting for more packets to complete message. Remaining size: %" PRIu32, reassembledMessage->remaining);
     return;
+  } else {
+    DEBUG_UDP(TAG, "[UDP] Reassembled complete message of size %" PRIu32, reassembledMessage->index);
   }
 
   // we have finished reassembling packets
@@ -232,22 +253,27 @@ static void onData(AsyncUDPPacket& packet) {
     delete reassembledMessage;
     reassembledMessage = nullptr;
     return;
+  } else {
+    DEBUG_UDP(TAG, "[UDP] CRC32 valid");
   }
 
   // extract message
   JsonDocument doc;
   DeserializationError err = reassembledMessage->parseMsgPack(doc);
 
-  // cleanup reassembled message buffer
-  delete reassembledMessage;
-  reassembledMessage = nullptr;
-
   if (err) {
     ESP_LOGD(TAG, "[UDP] Failed to parse MsgPack: %s", err.c_str());
+    delete reassembledMessage;
+    reassembledMessage = nullptr;
     return;
+  } else {
+    DEBUG_UDP(TAG, "[UDP] MsgPack parsed successfully");
   }
 
   processJSON(doc);
+
+  delete reassembledMessage;
+  reassembledMessage = nullptr;
 }
 
 void yasolr_configure_jsy_remote() {
